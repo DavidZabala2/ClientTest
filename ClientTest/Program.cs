af9edbe271ff9beee5df0b9e933ac8061256fa79
David <davideduardo.zabalacuellar@elev.ga.ntig.se>
2019-01-08 13:09:51 +0100

    @ntitobias ClientTest David Zabala

--- ClientTest/Program.cs
+++ ClientTest/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,29 @@ namespace ClientTest
     {
         static void Main(string[] args)
         {
+            string address = "127.0.0.1";
+            int port = 8001;
+
+            Console.WriteLine("Anluster...");
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(address, port);
+            Console.WriteLine("Ansluten!");
+
+            Console.Write("Skriv in meddelande: ");
+            string message = Console.ReadLine();
+
+            Byte[] bMessage = System.Text.Encoding.UTF8.GetBytes(message);
+
+            Console.WriteLine("Skickar...");
+            NetworkStream tcpStream = tcpClient.GetStream();
+            tcpStream.Write(bMessage, 0, bMessage.Length);
+
+            tcpClient.Close();
+
+
+
+
+
         }
     }
 }
