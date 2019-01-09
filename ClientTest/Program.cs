d2ec1c102a9f2ae666f4a4009a54fc98b6563b2e
David <davideduardo.zabalacuellar@elev.ga.ntig.se>
2019-01-09 10:35:08 +0100

    Gjort klart

--- ClientTest/Program.cs
+++ ClientTest/Program.cs
@@ -11,32 +11,43 @@ namespace ClientTest
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            string address = "127.0.0.1";
-            int port = 8001;
+            try
+            {
+                string address = "127.0.0.1";
+                int port = 8001;
 
-            Console.WriteLine("Anluster...");
-            TcpClient tcpClient = new TcpClient();
-            tcpClient.Connect(address, port);
-            Console.WriteLine("Ansluten!");
+                Console.WriteLine("Ansluter...");
+                TcpClient tcpClient = new TcpClient();
+                tcpClient.Connect(address, port);
+                Console.WriteLine("Ansluten!");
 
-            Console.Write("Skriv in meddelande: ");
-            string message = Console.ReadLine();
+                Console.Write("Skriv in meddelande: ");
+                String message = Console.ReadLine();
 
-            Byte[] bMessage = System.Text.Encoding.UTF8.GetBytes(message);
+                Byte[] bMessage = System.Text.Encoding.ASCII.GetBytes(message);
 
-            Console.WriteLine("Skickar...");
-            NetworkStream tcpStream = tcpClient.GetStream();
-            tcpStream.Write(bMessage, 0, bMessage.Length);
+                Console.WriteLine("Skickar...");
+                NetworkStream tcpStream = tcpClient.GetStream();
+                tcpStream.Write(bMessage, 0, bMessage.Length);
 
-            tcpClient.Close();
+                Byte[] bRead = new byte[256];
+                int bReadSize = tcpStream.Read(bRead, 0, bRead.Length);
 
-            IPAddress myIp = IPAddress.Parse("127.0.0.1");
+                string read = "";
+                for (int i = 0; i < bReadSize; i++)
+                    read += Convert.ToChar(bRead[i]);
 
+                Console.WriteLine("Servern säger: " + read);
 
-
-
-        }
+                tcpClient.Close();
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+    }
     }
 }
